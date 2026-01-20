91415801bf506b40de8b0db8e285a4f0a01d7694
no_one <135714413+amrmohamed125@users.noreply.github.com>
2026-01-20 22:38:18 +0200

    Change redirect URL to index.html on login
    Updated the redirect URL after successful login.

--- login/login.js
+++ login/login.js
@@ -15,13 +15,14 @@ login.addEventListener("click", function(e) {
 
     if (getemail.trim() === email.value.trim() && getpassword.trim() === password.value.trim()) {
         setTimeout(() => {
-            window.location.href = "/E-Commerce/index.html";
+            window.location.href = "/index.html";
         }, 1000);
     } else {
         alert("Incorrect email or password!");
     }
 
 });
+
 
 
 
